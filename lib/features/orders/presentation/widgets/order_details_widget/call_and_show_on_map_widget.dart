c3c3efba821c876a3763129221935fc71d5e40ac
w.i <mynaz7142@gmail.com>
2025-07-12 09:24:29 +0300

    refactor code

---
+++ lib/features/orders/presentation/widgets/order_details_widget/call_and_show_on_map_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:oborkom/core/helpers/extension.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_styles.dart';
+import '../../../../../generated/assets.dart';
+import '../../../../../generated/l10n.dart';
+
+class CallAndShowOnMapWidget extends StatelessWidget {
+  const CallAndShowOnMapWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: AppColors.greenColor.withAlpha(40),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  Assets.imagesPhoneCalling,
+                  colorFilter: const ColorFilter.mode(
+                    AppColors.greenColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                10.width,
+                Text(
+                  S.of(context).call,
+                  style: AppTextStyles.bold14Grey.copyWith(
+                    color: AppColors.greenColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        10.width,
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: AppColors.darkMainColor.withAlpha(40),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  Assets.imagesCompassBig,
+                  colorFilter: const ColorFilter.mode(
+                    AppColors.darkMainColor,
+                    BlendMode.srcIn,
+                  ),
+                ),
+                10.width,
+                Text(
+                  S.of(context).showOnMap,
+                  style: AppTextStyles.bold14Grey.copyWith(
+                    color: AppColors.darkMainColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/orders/presentation/widgets/order_details_widget/driver_location_widget.dart
+++ lib/features/orders/presentation/widgets/order_details_widget/driver_location_widget.dart
@@ -7,6 +7,7 @@ import '../../../../../core/utils/app_styles.dart';
 import '../../../../../generated/assets.dart';
 import '../../../../../generated/l10n.dart';
 import '../../../../profile/presentation/widgets/profile_screen_widgets/profile_image.dart';
+import 'call_and_show_on_map_widget.dart';
 
 class DriverDetails extends StatelessWidget {
   const DriverDetails({super.key});
@@ -77,69 +78,10 @@ class DriverDetails extends StatelessWidget {
             ],
           ),
           20.height,
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: AppColors.greenColor.withAlpha(40),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        Assets.imagesPhoneCalling,
-                        colorFilter: const ColorFilter.mode(
-                          AppColors.greenColor,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                      10.width,
-                      Text(
-                        S.of(context).call,
-                        style: AppTextStyles.bold14Grey.copyWith(
-                          color: AppColors.greenColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              10.width,
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: AppColors.darkMainColor.withAlpha(40),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        Assets.imagesCompassBig,
-                        colorFilter: const ColorFilter.mode(
-                          AppColors.darkMainColor,
-                          BlendMode.srcIn,
-                        ),
-                      ),
-                      10.width,
-                      Text(
-                        S.of(context).showOnMap,
-                        style: AppTextStyles.bold14Grey.copyWith(
-                          color: AppColors.darkMainColor,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+          const CallAndShowOnMapWidget(),
         ],
       ),
     );
   }
 }
+
